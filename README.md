fc392e9efba71be3d10b132faec2566b636b36b7
liliang <liliang_wk@163.com>
2020-01-30 21:24:45 +0800

    v0.1.0 done

---
+++ README.md
@@ -0,0 +1,44 @@
+# CodeBlock 代码生成器
+
+
+![](./arduinoC/_images/featured.png)
+
+---------------------------------------------------------
+
+
+## 链接
+- **本用户库加载地址:** ```https://github.com/liliang9693/ext-codeBlock```
+- **Mind+下载地址：** [http://mindplus.cc](http://mindplus.cc "http://mindplus.cc")    
+- **Mind+扩展库教程：** [https://mindplus.dfrobot.com.cn/extensions-user](https://mindplus.dfrobot.com.cn/extensions-user "https://mindplus.dfrobot.com.cn/extensions-user")    
+
+## 简介
+
+- **介绍：** 本扩展库实现在图形模块上输入代码，实现图形化积木与代码混合编写的目的，方便在没有图形积木时直接调用代码。  
+
+## Blocks
+
+![](./arduinoC/_images/block.png)
+
+## 示例
+![](./arduinoC/_images/example.png)
+
+
+
+## License
+
+MIT
+
+## 硬件支持
+
+MCU                | JavaScript    | Arduino   | MicroPython    | Remarks
+------------------ | :----------: | :----------: | :---------: | -----
+micro:bit        |             |       √       |             | 
+mpython        |             |        √      |             | 
+arduino uno    |             |        √      |             | 
+ 
+
+## 更新日志
+- V0.1.0  20200130 第一版发布
+
+
+
---
+++ arduinoC/main.ts
@@ -0,0 +1,134 @@
+
+//% color="#AA278D" iconWidth=50 iconHeight=40
+namespace CodeBlock {
+    //% block="#include <[STR]>"  blockType="command"
+    //% STR.shadow="string" STR.defl="Test.h"
+    export function include(parameter:any){
+        let str=parameter.STR.code;
+        if(str.substring(0,1)=="\"")  str=str.substring(1,str.length-1);
+        Generator.addInclude("CodeBlockInclude",`#include <${str}>`);
+
+        
+    }
+    //% block="#define [STR]"  blockType="command"
+    //% STR.shadow="string" STR.defl="MYDEFINE MDF"
+    export function define(parameter:any){
+        let str=parameter.STR.code;
+        if(str.substring(0,1)=="\"")  str=str.substring(1,str.length-1);
+        Generator.addInclude("CodeBlockDefine",`#define ${str}`);
+
+        
+    }
+    //% block="[STR]"  blockType="command"
+    //% STR.shadow="string" STR.defl="//#define include any"
+    export function defineAny(parameter:any){
+        let str=parameter.STR.code;
+        if(str.substring(0,1)=="\"")  str=str.substring(1,str.length-1);
+        Generator.addInclude("CodeBlockDefineAny",`${str}`);
+
+        
+    }
+  
+    //% block="[OBJ] [OBJNAME];"   blockType="command"
+    //% OBJ.shadow="string" OBJ.defl="Test"
+    //% OBJNAME.shadow="string" OBJNAME.defl="test"
+    export function object(parameter:any){
+        let obj=parameter.OBJ.code;
+        let name=parameter.OBJNAME.code;
+
+        if(obj.substring(0,1)=="\"")  obj=obj.substring(1,obj.length-1);
+        if(name.substring(0,1)=="\"")  name=name.substring(1,name.length-1);
+ 
+        Generator.addObject("CodeBlockObject",`${obj}`,`${name}`);
+
+        
+    }
+
+
+    //% block="[OBJ]"   blockType="command"
+    //% OBJ.shadow="string" OBJ.defl="//object any"
+
+    export function objectAny(parameter:any){
+        let obj=parameter.OBJ.code;
+
+        if(obj.substring(0,1)=="\"")  obj=obj.substring(1,obj.length-1);
+
+ 
+        Generator.addObject("CodeBlockObjectAny"," ",`${obj}`);
+
+        
+    }
+
+    //% block="setUp:[SETUP]"   blockType="command"
+    //% SETUP.shadow="string" SETUP.defl="//setup any;"
+
+    export function setupAny(parameter:any){
+        let code=parameter.SETUP.code;
+
+        if(code.substring(0,1)=="\"")  code=code.substring(1,code.length-1);
+
+ 
+        Generator.addSetup("CodeBlockSetupAny",` ${code}`,``);
+
+        
+    }
+    //% block="Code:[CODE]"   blockType="command"
+    //% CODE.shadow="string" CODE.defl="//code any;"
+
+    export function codeAny(parameter:any){
+        let code=parameter.CODE.code;
+    
+        if(code.substring(0,1)=="\"") code=code.substring(1,code.length-1);
+
+ 
+        Generator.addCode(`${code};`);
+
+        
+    }
+    //% block="function Declare: [OUTPUT] [FUNCTION]([INPUT]){[CODE]}" blockType="command"
+    //% OUTPUT.shadow="string" OUTPUT.defl="float"
+    //% FUNCTION.shadow="string" FUNCTION.defl="functionName"
+    //% INPUT.shadow="string" INPUT.defl="float varf,float vars"
+    //% CODE.shadow="string" CODE.defl="return varf+vars;"
+    export function functionDeclare(parameter:any){
+        let out=parameter.OUTPUT.code;
+        let fct=parameter.FUNCTION.code;
+        let input=parameter.INPUT.code;
+        let cd=parameter.CODE.code;
+
+        if(out.substring(0,1)=="\"")  out=out.substring(1,out.length-1);
+        if(fct.substring(0,1)=="\"")  fct=fct.substring(1,fct.length-1);
+        if(input.substring(0,1)=="\"")  input=input.substring(1,input.length-1);
+        if(cd.substring(0,1)=="\"")  cd=cd.substring(1,cd.length-1);
+
+        Generator.addObject("CodeBlockFunctionDeclare",`${out}`,`${fct}(${input}){${cd}}`);    
+    }
+
+
+    //% block="Function call with return value: [FUNCTION]([INPUT])" blockType="reporter"
+    //% FUNCTION.shadow="string" FUNCTION.defl="functionName"
+    //% INPUT.shadow="string" INPUT.defl="1,1"
+    export function functionWithReturn(parameter:any){
+        let fct=parameter.FUNCTION.code;
+        let input=parameter.INPUT.code;
+
+        if(fct.substring(0,1)=="\"")  fct=fct.substring(1,fct.length-1);
+        if(input.substring(0,1)=="\"")  input=input.substring(1,input.length-1);
+
+        Generator.addCode(`${fct}(${input})`);    
+    }
+
+    //% block="Function call without return value: [FUNCTION]([INPUT]);" blockType="command"
+    //% FUNCTION.shadow="string" FUNCTION.defl="functionName"
+    //% INPUT.shadow="string" INPUT.defl="1,1"
+    export function functionWithoutReturn(parameter:any){
+        let fct=parameter.FUNCTION.code;
+        let input=parameter.INPUT.code;
+
+        if(fct.substring(0,1)=="\"")  fct=fct.substring(1,fct.length-1);
+        if(input.substring(0,1)=="\"")  input=input.substring(1,input.length-1);
+
+        Generator.addCode(`${fct}(${input});`);    
+    }
+   
+}
